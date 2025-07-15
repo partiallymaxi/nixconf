1a59d6c80621cbd44afcaf2348428ec8b8578616
Maxi Saparov <maxi.saparov@gmail.com>
2025-07-15 15:43:26 -0400

    typo

--- home/common/programs/core/fastfetch.nix
+++ home/common/programs/core/fastfetch.nix
@@ -3,7 +3,7 @@ with lib;
 {
 
   config = mkIf config.programs.core.enable {
-    fastfetch = {
+    programs.fastfetch = {
       enable = true;
       settings = {
         schema = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
